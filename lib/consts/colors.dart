fcb76e778edd2c29df76e619c76b82a4f32c1bca
Rohit Bharat Bhandwalkar <rohitb.bhandwalkar@gmail.com>
2024-02-24 12:36:50 +0530

    Code card for event announcement

--- lib/consts/colors.dart
+++ lib/consts/colors.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
-Color primary_1 = const Color(0xff4C5C68);
-Color primary_2 = const Color(0xff2E3438);
-Color primary_3 = const Color(0xff191A1B);
+// Color primary_1 = const Color(0xff4C5C68);
+// Color primary_2 = const Color(0xff2E3438);
+// Color primary_3 = const Color(0xff191A1B);
 
-Color secondary_1 = const Color(0xff247BA0);
-Color secondary_2 = const Color(0xff08587A);
-Color secondary_3 = const Color(0xff033F58);
+// Color secondary_1 = const Color(0xff247BA0);
+// Color secondary_2 = const Color(0xff08587A);
+// Color secondary_3 = const Color(0xff033F58);
 
-Color accent_1 = const Color(0xffDE2F3B);
-Color accent_2 = const Color(0xffC31420);
-Color accent_3 = const Color(0xff8D030D);
+// Color accent_1 = const Color(0xffDE2F3B);
+// Color accent_2 = const Color(0xffC31420);
+// Color accent_3 = const Color(0xff8D030D);
+
+Color primary_1 = const Color(0xffffffff);
+Color primary_2 = const Color(0xfff2f2f2);
+Color primary_3 = const Color(0xffbfbfbf);
+
+Color secondary_1 = const Color(0xff9a5ebb);
+Color secondary_2 = const Color(0xff64357d);
+Color secondary_3 = const Color(0xff2b1736);
+
+Color accent_1 = const Color(0xff268d80);
+Color accent_2 = const Color(0xff103c37);
+Color accent_3 = const Color(0xff103c37);
